4c5f0e5032a7d88047404ba28905c2f000f963fe
softhunter-tusur <softhunter@mail.ru>
2024-02-18 17:06:37 +0300

    Commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,31 +1,37 @@
+import os
 import unittest
-
-from app import app
-
+from flask import Flask
+from werkzeug.datastructures import FileStorage
 
 class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
+        # Создаем экземпляр приложения Flask для тестирования
+        self.app = Flask(__name__)
+        # Включаем режим тестирования
+        self.app.config['TESTING'] = True
 
     def tearDown(self):
         pass
 
-    def test_index(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('Добавление рамки и построение графика распределения цветов'.encode('utf-8'), response.data)
-
     def test_upload_image(self):
-        with open('test_image.jpg', 'rb') as f:
-            response = self.app.post('/', data={'image': (f, 'test_image.jpg'), 'border_size': 10},
-                                     follow_redirects=True)
-            self.assertEqual(response.status_code, 200)
-            self.assertIn(b'Result', response.data)
-            self.assertIn(b'bordered_test_image.jpg', response.data)
-            self.assertIn(b'color_distribution.png', response.data)
+        # Определяем путь к тестовому изображению
+        test_image_path = os.path.join(os.path.dirname(__file__), 'test_image.jpg')
 
+        # Открываем файл с изображением
+        with open(test_image_path, 'rb') as f:
+            # Создаем объект FileStorage, представляющий загружаемый файл
+            file_storage = FileStorage(f)
+
+            # Отправляем POST-запрос на сервер с загружаемым файлом и другими данными
+            with self.app.test_client() as client:
+                response = client.post('/', data={'image': file_storage, 'border_size': 10},
+                                       follow_redirects=True)
+
+                # Проверяем код ответа
+                self.assertEqual(response.status_code, 200)
+                # Проверяем, что в ответе есть ожидаемый текст "Result"
+                self.assertIn(b'Result', response.data)
 
 if __name__ == '__main__':
     unittest.main()
