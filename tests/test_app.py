24dc1ea9697ab418219326b4f2c5ee7cd18d2ce0
softhunter-tusur <softhunter@mail.ru>
2024-02-18 16:12:30 +0300

    Commit

---
+++ tests/test_app.py
@@ -0,0 +1,31 @@
+import unittest
+
+from app import app
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_index(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Добавление рамки и построение графика распределения цветов'.encode('utf-8'), response.data)
+
+    def test_upload_image(self):
+        with open('test_image.jpg', 'rb') as f:
+            response = self.app.post('/', data={'image': (f, 'test_image.jpg'), 'border_size': 10},
+                                     follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Result', response.data)
+            self.assertIn(b'bordered_test_image.jpg', response.data)
+            self.assertIn(b'color_distribution.png', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
